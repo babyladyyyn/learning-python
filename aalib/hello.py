e36fdf4dece73dc62166db187a3a7433959640b2
unknown <shimei@meizu.com>
2017-08-24 17:36:45 +0800

    ok

--- aalib/hello.py
+++ aalib/hello.py
@@ -1,10 +1,13 @@
 import io
-import urllib2
+
 import PIL.Image
+import urllib2
+
 import aalib
 
 screen = aalib.AsciiScreen(width=80, height=40)
 fp = io.BytesIO(urllib2.urlopen('https://www.python.org/static/favicon.ico').read())
 image = PIL.Image.open(fp).convert('L').resize(screen.virtual_size)
 screen.put_image((0, 0), image)
-print screen.render()
+print
+screen.render()
---
+++ cmd/test_arg_to_dict.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestArg_to_dict(TestCase):
+    pass
